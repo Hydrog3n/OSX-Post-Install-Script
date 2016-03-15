d9ac50959bde2a3d6371815bce51774a2605463d
hydrog3n <loic.vaille@gmail.com>
2016-03-15 22:20:32 +0100

    add 1password

--- install.sh
+++ install.sh
@@ -371,7 +371,7 @@ brew cask install balsamiq-mockups --appdir=/Applications
 #brew cask install pencil
 
 ## SYSTEM UTILITIES 
-brew cask install disk-inventory-x moom unetbootin istat-menus caffeine cyberduck rescuetime the-unarchiver --appdir=/Applications
+brew cask install 1password disk-inventory-x moom unetbootin istat-menus caffeine cyberduck rescuetime the-unarchiver --appdir=/Applications
 
 ## VIRTUALIZATION / CONTAINERS
 ###Removed these by default in case you're deploying a VM
